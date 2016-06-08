feff461fde52421ad606001eb36e7e661c6c5738
tolkadot <tolkadot@gmail.com>
2016-06-08 02:23:56 +0000

    htaccess

--- functions.php
+++ functions.php
@@ -172,7 +172,17 @@ function addGoogleFonts() {
 }
 
 
+
+
+
+
+// Add redirect to prevent disclosing the admin username
+add_action(‘template_redirect’, ‘bwp_template_redirect’);
+function bwp_template_redirect()
+{
+if (is_author())
+{
+wp_redirect( home_url() ); exit;
+}
+}
 ?>
-
-
-
---
+++ single.php
@@ -0,0 +1,48 @@
+<?php 
+
+/* Add a class to the blog page body */
+add_filter( 'body_class', 'dodder_body_class' );
+function dodder_body_class( $classes ) {
+	
+	$classes[] = 'blogPageBody';
+	return $classes;
+}
+
+/* Add a class to the blog page site inner body */
+/*this is a function to add classes */
+function dodder_add_class( $attr, $class ) {
+    $attr['class'] .= ' ' . sanitize_html_class( $class );
+    return $attr;
+}
+
+add_filter( 'genesis_attr_site-inner', 'dodder_site_inner_class' );
+function dodder_site_inner_class( $attr ) {
+    return dodder_add_class( $attr, 'blogPageSiteInnerClass' );
+}
+
+/* add a sidebar to the blog */
+add_filter( 'genesis_pre_get_option_site_layout', 'dodder_sidebar_blog' );
+
+function dodder_sidebar_blog ( $opt ) {
+if ( is_home() ) {
+$opt = 'content-sidebar';
+return $opt;
+}
+}
+
+/* add a class to the content area */
+add_filter( 'genesis_attr_content', 'dodder_content_class' );
+function dodder_content_class( $attr ) {
+    return dodder_add_class( $attr, 'blogContentClass' );
+}
+/* add a class to the content-sidebar wrap area */
+add_filter( 'genesis_attr_content-sidebar-wrap', 'dodder_content_sidebar_wrap' );
+function dodder_content_sidebar_wrap( $attr ) {
+    return dodder_add_class( $attr, 'blogContentSidebarWrapClass' );
+}
+
+genesis();
+
+
+
+?>
